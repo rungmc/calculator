3bd175e5890a5e6cbe048fce5e02e8b4313bfde7
rungmc <gregmcm@gmail.com>
2021-09-14 20:35:58 -0400

    Most functions operational.

--- README.md
+++ README.md
@@ -10,6 +10,5 @@ To create a functioning old-school-style calculator using combined knowledge of 
 - [x] Add number button grid (with `.` and `=` keys to round out a 3x4)
 - [x] Add basic operator buttons (add, subtract, multiply, divide)
 - [ ] Program operators
-- [ ] PEMDAS
-- [ ] MRC/M+/M- and added goodies
+- [x] MRC/M+/M- and added goodies
 - [x] Final look & feel formatting (80s inspired theming, "Calcio")
--- calc.js
+++ calc.js
@@ -1,5 +1,7 @@
 let memory = 0;
-let display = '80085';
+let display = '0';
+let oldDisplay = '';
+let operand = '';
 
 const buttons = document.querySelectorAll('button');
 buttons.forEach(button => {
@@ -8,50 +10,46 @@ buttons.forEach(button => {
         button.classList.add('button-click');
         setTimeout(function(){button.classList.remove('button-click')},300);
 
-        if(button.id == 'clear') clear();
-        else if(button.id == 'ac') allClear();
-        else if(button.id == 'off') turnOff();
-        else if(button.id == 'neg') negative();
-        else if(button.id == 'percent') percent();
+        if(button.classList.contains('operator')){
+            if(button.id == 'clear') clear();
+            else if(button.id == 'ac') allClear();
+            else if(button.id == 'off') turnOff();
+            else if(button.id == 'neg') negative();
+            else if(button.id == 'percent') percent();
 
-        else if(button.id == 'mrc') memRecall();
-        else if(button.id == 'mplus') memPlus();
-        else if(button.id == 'mminus') memMinus();
+            else if(button.id == 'mrc') memRecall();
+            else if(button.id == 'mplus') memPlus();
+            else if(button.id == 'mminus') memMinus();
+        }
+        else if(button.classList.contains('operand')){
+            operand = button.innerText;
+            oldDisplay = display;
+            display = '';
+        }
+        //Prevents user entry of double decimals.
+        else if(button.id !== 'dec' || !display.includes('.')){
+            if (display == '0') display = '';
+            display += button.innerText;
+            updateDisplay();
+        }
     });
 });
 
 function updateDisplay() {
     const displayScreen = document.querySelector('#display');
-    if(display.length > 10) displayScreen.innerText = display.substring(0,9)+'...';
+    
+    //Rounds if scientific notation kicks in.
+    if (display.includes('e')) {
+        display = display.substring(0,5)
+                + display.substring(display.indexOf('e'));
+        displayScreen.innerText = display;
+    }
+    //Trailing ellipses if number is too long for display.
+    else if(display.length > 10) displayScreen.innerText = display.substring(0,9)+'...';
     else displayScreen.innerText = display;
 }
 
 updateDisplay();
-
-
-
-function equals(){
-
-}
-
-function add(num1, num2){
-    display = num1+num2;
-    updateDisplay();
-}
-
-function subtract(num1, num2){
-    display = num1-num2;
-    updateDisplay();
-}
-
-function multiply(num1, num2){
-    display = num1*num2;
-    updateDisplay();
-}
-
-function divide(num1, num2){
-
-}
 
 function percent(){
     display = (parseFloat(display)*0.01).toString();
